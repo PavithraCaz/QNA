048af8d331bbebb09951958825934dcd834ccbb8
PavithraCaz <pavithracaz@gmail.com>
2022-03-02 15:13:06 +0530

    second commit

--- QNA/urls.py
+++ QNA/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Landing.urls')),
+    path('landing/',include('Landing.urls')),
     path('',include('Home.urls')),
 ]
