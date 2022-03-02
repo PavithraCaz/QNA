90d98642a5cd8c6a170ec63e93e62f19903ee7ab
PavithraCaz <pavithracaz@gmail.com>
2022-03-02 15:07:48 +0530

    secon commit

--- Landing/views.py
+++ Landing/views.py
@@ -4,8 +4,9 @@ from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
-def land2(request):
-    return render(request,'Login.html')
-    
 def land1(request):
     return render(request,'Signup.html')
+    
+
+def land2(request):
+    return render(request,'Login.html')
